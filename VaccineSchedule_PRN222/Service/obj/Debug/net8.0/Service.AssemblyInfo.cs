daaf145cbe1b13be470b73856b4fb288df8f4cde
Dixeliare <daoxuanlong492004@gmail.com>
2025-04-02 20:11:32 +0700

    Update UI for staff , admin and customer

--- VaccineSchedule_PRN222/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ VaccineSchedule_PRN222/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269d87b652a81e4c0c2d22cee0e903c742fb5335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01a3e50dadfdaa99d396f00116fc2f184772009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
